a3e6de9360f5456034b5065756c0a34fd3c35dbe
goodjeon <513ehdwnsl@gmail.com>
2023-01-03 21:08:44 +0900

    자바1

---
+++ JavaWorkspace/src/chap_01/_02_DataTypes.java
@@ -0,0 +1,16 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("안녕하세요?");
+        System.out.println(12);
+        System.out.println(-34);
+        System.out.println(3.141592);
+        System.out.println(true);
+        System.out.println(false);
+
+        System.out.println(123 + 345);
+        System.out.println(1+1.1);
+    }
+}
